e95b63ddbc313da1270563ff038a000ffb122725
Mbali-N <123359086+Mbali-N@users.noreply.github.com>
2023-03-28 11:37:08 +0200

    Update align_handler.c

--- align_handler.c
+++ align_handler.c
@@ -19,3 +19,58 @@ int put_string(char *str)
 	}
 	return (i);
 }
+
+/**
+ *print_right - invokes right alignment with padding options
+ *@str:no. in a string or no. to print
+ *@handler:handler struct
+ *Return:no. of characters printed
+ */
+int print_right(char *str, han_s *handler)/*prints in a normal alignment*/
+{
+	unsigned int d = 0, neg, neg1;
+	unsigned int len = strlen(str);
+
+	char pad = ' ';
+
+/*number to be padded with zeroes left align not invoked*/
+/*to invoke zero leading fill*/
+	if (handler->zero && !handler->minus)
+	{
+		pad = '0';
+	}
+/*checks whether number is unsigned*/
+	neg = (*str == '-');
+	neg1 = neg;
+/*unsigned and width specified and pad = '0'*/
+/*incrementing the str address*/
+	if (neg && len < handler->width && !handler->minus && pad == '0')
+		str++;
+	else
+		neg = 0;
+/*evaluates to invisible + or +*/
+	if ((handler->plus && !neg1) || (!handler->plus && handler->space && !neg1))
+		len++;
+	if (neg && pad == '0')/*print a negative sign*/
+		d += _putchar('-');
+	if (handler->plus && !neg1 && pad == '0')/*evaluates to '+'*/
+		d += _putchar('+');
+/*handling pad printing*/
+
+	for (len = len + 1; len < handler->width; )
+	{
+		d += _putchar(pad);
+	}
+	if (neg && pad == ' ')
+	{
+		d += _putchar('-');
+	}
+	if (handler->plus && !neg1 && pad == ' ')
+	{
+		d += _putchar('+');
+	}
+	else if (!handler->plus && handler->space && !neg1 && !handler->zero)
+		d += _putchar(' ');
+	d += put_string(str);
+	return (d);
+}
