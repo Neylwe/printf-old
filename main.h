c915137f14ee994c1c474d8df039b49bbba7f665
Neylwe <keneilwe.mosala@ymail.com>
2023-03-28 05:14:17 -0700

    Update script

--- main.h
+++ main.h
@@ -76,14 +76,16 @@ int print_Hex(va_list list);
 int print_S(va_list list);
 int *rev_string(char *s);
 char *hex_conv(int num);
-int print_address(va_list list);
+int print_p(va_list list);
 int print_unsigned(va_list list);
 int print_rot13(va_list list);
 void _write(buffer *b_r, char c);
 void _write_str(buffer *b_r, char *s);
 void _write_tmpbuf(buffer *b_r);
 int print_r(va_list list);
-
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
 
 
 
